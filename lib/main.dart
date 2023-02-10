97cb03049a980338a0388ca6aa00a889b3ad6f0f
naufalrays <rayhannaufalherlano@gmail.com>
2023-02-10 16:31:06 +0700

    feature: add file

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,8 @@ import 'package:permission_handler/permission_handler.dart';
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Permission.camera.request();
-  await Permission.mediaLibrary.request();
-  await Permission.photos.request();
-  await Permission.storage.request();
 
   if (Platform.isAndroid) {
-    print("dijalankan");
     await AndroidInAppWebViewController.setWebContentsDebuggingEnabled(true);
 
     var swAvailable = await AndroidWebViewFeature.isFeatureSupported(
@@ -28,7 +24,6 @@ Future main() async {
       await serviceWorkerController
           .setServiceWorkerClient(AndroidServiceWorkerClient(
         shouldInterceptRequest: (request) async {
-          print("jalan");
           return null;
         },
       ));
@@ -45,16 +40,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const WebViewPage(),
--- lib/webview_page.dart
+++ lib/webview_page.dart
@@ -2,7 +2,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewPage extends StatefulWidget {
   const WebViewPage({super.key});
@@ -12,51 +11,27 @@ class WebViewPage extends StatefulWidget {
 }
 
 class _WebViewPageState extends State<WebViewPage> {
-  WebViewController? _controller;
   InAppWebViewController? _webViewController;
 
   @override
   void initState() {
     super.initState();
-    // await AndroidInAppWebViewController.setWebContentsDebuggingEnabled(true);
+  }
 
-    //   _controller = WebViewController()
-    //     ..setJavaScriptMode(JavaScriptMode.unrestricted)
-    //     ..setBackgroundColor(const Color(0x00000000))
-    //     ..setNavigationDelegate(
-    //       NavigationDelegate(
-    //         onProgress: (int progress) async {
-    //           PermissionStatus cameraStatus = await Permission.camera.request();
-    //           // Update loading bar.
-
-    //           if (cameraStatus == PermissionStatus.granted) {
-    //             print("diizinkan");
-    //           }
-    //         },
-    //         onPageStarted: (String url) {},
-    //         onPageFinished: (String url) {},
-    //         onWebResourceError: (WebResourceError error) {},
-    //         onNavigationRequest: (NavigationRequest request) {
-    //           if (request.url.startsWith('https://www.youtube.com/')) {
-    //             return NavigationDecision.prevent;
-    //           }
-    //           return NavigationDecision.navigate;
-    //         },
-    //       ),
-    //     )
-    //     ..loadRequest(Uri.parse('https://clever-peony-3af47e.netlify.app/'));
-    // }
+  @override
+  void dispose() {
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Web View"),
+        title: const Center(child: Text("Web View Camera")),
       ),
       body: InAppWebView(
-          initialUrlRequest: URLRequest(
-              url: Uri.parse("https://clever-peony-3af47e.netlify.app/")),
+          initialUrlRequest:
+              URLRequest(url: Uri.parse("https://mobilenet-tfjs.netlify.app/")),
           initialOptions: InAppWebViewGroupOptions(
             crossPlatform: InAppWebViewOptions(
               mediaPlaybackRequiresUserGesture: false,
